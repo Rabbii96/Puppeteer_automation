b6f95ec5e7e21340e313143443fb2bc8bef8a18a
rabbi <frabbif96@gmail.com>
2024-04-15 11:09:47 +0600

    update1 commit

--- intro.mjs
+++ intro.mjs
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import { setTimeout } from "timers/promises"
 
 const browser = await puppeteer.launch({
     headless: false,
@@ -8,22 +9,50 @@ const browser = await puppeteer.launch({
 });
 
 const page = await browser.newPage()
-await page.goto("https://example.com/",{
-    waitUntil: "networkidle0",
-    setTimeout: 3000,
-})
-await page.screenshot({path: "exam.com.png"})
-
 await page.goto("https://duckduckgo.com", {waitUntil: 'networkidle2'})
 await page.waitForSelector("#searchbox_homepage")
-await page.type("#searchbox_homepage",'Rabbii96')
+await page.type("#searchbox_homepage",'https://devconfbd.com/')
 
 const searchButtonHandel = await page.waitForSelector("button[aria-label='Search']", { timeout: 20000 });
 await searchButtonHandel.click()
 
-const firstResult = await page.waitForSelector("//span[normalize-space()='Rabbii96 (Rabbii96) · GitHub']")
+
+const firstResult = await page.waitForSelector('[data-testid="result-title-a"]')
 await firstResult.click()
 
-await firstResult.screenshot({ path: "duckduckgo1.png"})
+const guestElement = await page.waitForSelector("img[alt='guest']")
+await guestElement.scrollIntoView();
+await setTimeout(1000)
 
+await guestElement.click("img[alt='guest']")
+await setTimeout(1000)
+
+await page.screenshot({path: "guest.png"});
 await browser.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const firstResult = await page.waitForSelector("//span[normalize-space()='Rabbii96 (Rabbii96) · GitHub']")
+// await firstResult.click()
+
+// await firstResult.screenshot({ path: "duckduckgo1.png"})
+
