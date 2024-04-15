f3d681008961804ec57a6287357022d7aab1dfde
rabbi <frabbif96@gmail.com>
2024-04-15 15:14:20 +0600

    update1 commit

--- intro.mjs
+++ intro.mjs
@@ -1,39 +1,71 @@
+import PQueue from "p-queue";
 import puppeteer from "puppeteer";
-import { setTimeout } from "timers/promises"
 
 const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width: 1920, height: 1080},
-    slowMo: 170,
+    // slowMo: 170,
     userDataDir: "Data"
 });
 
 const page = await browser.newPage()
 await page.goto("https://duckduckgo.com", {waitUntil: 'networkidle2'})
 await page.waitForSelector("#searchbox_homepage")
-await page.type("#searchbox_homepage",'https://devconfbd.com/')
+await page.type("#searchbox_homepage",'devconfbd')
 
 const searchButtonHandel = await page.waitForSelector("button[aria-label='Search']", { timeout: 20000 });
 await searchButtonHandel.click()
 
 
 const firstResult = await page.waitForSelector('[data-testid="result-title-a"]')
-await firstResult.click()
+await firstResult.click();
 
-const guestElement = await page.waitForSelector("img[alt='guest']")
-await guestElement.scrollIntoView();
-await setTimeout(1000)
+await page.waitForSelector('.sponsors a, .supporter a');
+const sponsorslinks= await page.evaluate(()=>{
+return [...document.querySelectorAll('.sponsors a')].map(e=>e.href)
 
-await guestElement.click("img[alt='guest']")
-await setTimeout(1000)
+})
+const supporterlinks= await page.evaluate(()=>{
+return [...document.querySelectorAll('.supporter a')].map(e=>e.href)
 
-await page.screenshot({path: "guest.png"});
+})
+
+console.log({sponsorslinks, supporterlinks});
+
+
+async function getLink(link){
+    const page = await browser.newPage()
+    await page.goto(link, {waitUntil: 'networkidle2'})
+    const title = await page.title()
+    const hostname = await page.evaluate(()=>window.location.hostname)
+    await page.screenshot({path: '${hostname}.png'})
+   const facebookLink = await page.evaluate(()=>document.querySelector('a[href*=facebook]')?.href)
+    console.log({link, title, hostname, facebookLink})
+    await page.close();
+}
+const queue = new PQueue({concurrency: 2})
+
+for (let link of supporterlinks){
+    queue.add(()=>getLink(link)).catch(console.log)
+}
+
+await queue.onEmpty()
+
+
 await browser.close();
 
 
 
 
 
+// const guestElement = await page.waitForSelector("img[alt='guest']")
+// await guestElement.scrollIntoView();
+// await setTimeout(1000)
+
+// await guestElement.click("img[alt='guest']")
+// await setTimeout(1000)
+
+// await page.screenshot({path: "guest.png"});
 
 
 
@@ -47,12 +79,4 @@ await browser.close();
 
 
 
-
-
-
-
-// const firstResult = await page.waitForSelector("//span[normalize-space()='Rabbii96 (Rabbii96) · GitHub']")
-// await firstResult.click()
-
-// await firstResult.screenshot({ path: "duckduckgo1.png"})
 
