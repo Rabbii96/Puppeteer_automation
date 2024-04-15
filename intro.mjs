3adde55ddc28fbe05f70bbb228f63db22089df8d
rabbi <frabbif96@gmail.com>
2024-04-15 09:41:46 +0600

    update commit

--- intro.mjs
+++ intro.mjs
@@ -14,4 +14,16 @@ await page.goto("https://example.com/",{
 })
 await page.screenshot({path: "exam.com.png"})
 
+await page.goto("https://duckduckgo.com", {waitUntil: 'networkidle2'})
+await page.waitForSelector("#searchbox_homepage")
+await page.type("#searchbox_homepage",'Rabbii96')
+
+const searchButtonHandel = await page.waitForSelector("button[aria-label='Search']", { timeout: 20000 });
+await searchButtonHandel.click()
+
+const firstResult = await page.waitForSelector("//span[normalize-space()='Rabbii96 (Rabbii96) · GitHub']")
+await firstResult.click()
+
+await firstResult.screenshot({ path: "duckduckgo1.png"})
+
 await browser.close();
